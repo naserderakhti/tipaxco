9f00a30f37f201edf244b0417f01471e3771d9ba
naser derakhti <naserderakhti@gmail.com>
2025-09-01 20:09:55 +0330

    added menu Icon next to logo from react icons

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router";
 import logoImg from "/images/logo.svg";
-import { CiMenuBurger } from "react-icons/ci";
+import { IoMenuOutline } from "react-icons/io5";
+
 export default function Nav() {
   return (
     <nav className="bg-baseGreen/95">
-      <div className="container flex flex-row justify-between">
+      <div className="container px-8 flex flex-row justify-between">
         <div className="w-1/4">login info</div>
-        <div className="outline-2 outline-amber-400 py-3 w-1/4 self-end flex flex-row justify-end">
+        <div className="py-3 w-1/4 self-end flex flex-row justify-around items-center">
           <Link to="/" end>
             <img src={logoImg} alt="logo" className="w-32" />
           </Link>
+          <IoMenuOutline className="text-white text-5xl cursor-pointer" />
         </div>
       </div>
     </nav>
