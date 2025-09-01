7d9c0a93b798a7bc5200b4422d0129bbd7080050
naser derakhti <naserderakhti@gmail.com>
2025-09-01 15:15:25 +0330

    created nav menu and give it some styles

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router";
+import logoImg from "/images/logo.svg";
+import { CiMenuBurger } from "react-icons/ci";
 export default function Nav() {
-  return <nav>nav component</nav>;
+  return (
+    <nav className="bg-baseGreen/95">
+      <div className="container flex flex-row justify-between">
+        <div className="w-1/4">login info</div>
+        <div className="outline-2 outline-amber-400 py-3 w-1/4 self-end flex flex-row justify-end">
+          <Link to="/" end>
+            <img src={logoImg} alt="logo" className="w-32" />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
