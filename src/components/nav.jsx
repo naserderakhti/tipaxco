27e62e37ab7b19a6ab5d979e6af631e900e77690
naser derakhti <naserderakhti@gmail.com>
2025-09-01 21:44:17 +0330

    added phone and search icons

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router";
 import logoImg from "/images/logo.svg";
 import { IoMenuOutline } from "react-icons/io5";
+import NavLoginSection from "./NavLoginSection";
 
 export default function Nav() {
   return (
     <nav className="bg-baseGreen/95">
-      <div className="container px-8 flex flex-row justify-between">
-        <div className="w-1/4">login info</div>
+      <div className="container md:px-8 flex flex-row justify-between">
+        <NavLoginSection />
         <div className="py-3 w-1/4 self-end flex flex-row justify-around items-center">
           <Link to="/" end>
             <img src={logoImg} alt="logo" className="w-32" />
